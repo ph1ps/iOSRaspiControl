d3445c6f278e41c6ce4aaa004439077ffa5dba27
ph1ps <gabriel.phi@gmx.at>
2016-12-22 11:39:42 +0100

    Fixed frameworks

--- iOSRaspiControl/ViewController.swift
+++ iOSRaspiControl/ViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import SwiftSocket
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let client = TCPClient(address: "www.apple.com", port: 80)
     }
 
     override func didReceiveMemoryWarning() {
